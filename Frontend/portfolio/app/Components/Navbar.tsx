49761f109c3439343911245947f3e7aad7d518bd
SuperexMack <unilexerox@gmail.com>
2026-02-03 12:38:48 +0530

    Done with the toggle

--- Frontend/portfolio/app/Components/Navbar.tsx
+++ Frontend/portfolio/app/Components/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Link from "next/link";
 import { Github,Moon } from 'lucide-react';
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { ChartNoAxesGantt } from 'lucide-react';
 
 export function Navbar({blogRef,projectRef,achRef}:any){
 
-    
+    const [clicked,setClicked] = useState(false)
 
     const blog = ()=>{
        if(blogRef.current){
@@ -34,17 +35,53 @@ export function Navbar({blogRef,projectRef,achRef}:any){
         }
     }
 
+    const toogleLogic = ()=>{
+        setClicked(!clicked)
+        console.log("Value of toggle is " + clicked)
+    }
+
     return(
         <>
         <div className="w-full z-50 fixed top-0 h-[80px] bg-black p-3 flex justify-center items-center space-x-6 border-b border-t border-gray-900">
 
-            <Link href={"/"}><h1 className="font-bold text-white">Portfolio</h1></Link>
-            <Link onClick={blog} href={"/"}><h1 className="hover:text-slate-400 font-bold text-white">Blogs</h1></Link>
-            <Link onClick={project} href={"/"}><h1 className="hover:text-slate-400 font-bold text-white">Projects</h1></Link>
-            <Link onClick={achv} href={"/"}><h1 className="hover:text-slate-400 font-bold text-white">achievement</h1></Link>
-            <Link href={"https://drive.google.com/file/d/1fZFqsCsYYfehO3txS3RXTL8OPxJT6mKy/view?usp=sharing"}><h1 className="hover:text-slate-400 font-bold text-white">Resume</h1></Link>
-            <Link href={"https://github.com/SuperexMack/jobportfolio"}><Github className="text-white"></Github></Link>
+
+            <div className="absolute left-[70px]">
+            <Link href={"/"}><h1 className="font-bold text-[25px] text-white">Portfolio</h1></Link>
+            </div>
+
+             {clicked?(
+
+               
+                <div className="flex w-full h-[200px] mt-[300px] p-2 bg-black flex-col space-y-3 items-center">
+                <Link onClick={blog} href={"/"}><h1 className="hover:text-slate-400 md:flex font-bold text-white">Blogs</h1></Link>
+                <Link onClick={project} href={"/"}><h1 className="hover:text-slate-400 md:flex font-bold text-white">Projects</h1></Link>
+                <Link onClick={achv} href={"/"}><h1 className="hover:text-slate-400 md:flex font-bold text-white">achievement</h1></Link>
+                <Link href={"https://drive.google.com/file/d/1fZFqsCsYYfehO3txS3RXTL8OPxJT6mKy/view?usp=sharing"}><h1 className="hover:text-slate-400 md:flex font-bold text-white">Resume</h1></Link>
+                <Link href={"https://github.com/SuperexMack/jobportfolio"}><Github className="text-white md:flex"></Github></Link>
+                </div>
+                
+
+             ):(
+                <>
+                <Link onClick={blog} href={"/"}><h1 className="hover:text-slate-400 md:flex hidden font-bold text-white">Blogs</h1></Link>
+                <Link onClick={project} href={"/"}><h1 className="hover:text-slate-400 md:flex hidden font-bold text-white">Projects</h1></Link>
+                <Link onClick={achv} href={"/"}><h1 className="hover:text-slate-400 md:flex hidden font-bold text-white">achievement</h1></Link>
+                <Link href={"https://drive.google.com/file/d/1fZFqsCsYYfehO3txS3RXTL8OPxJT6mKy/view?usp=sharing"}><h1 className="hover:text-slate-400 md:flex hidden font-bold text-white">Resume</h1></Link>
+                <Link href={"https://github.com/SuperexMack/jobportfolio"}><Github className="text-white md:flex hidden"></Github></Link>
+                </>
+             )}
+
+           
+         
+        <div className="absolute right-[80px] md:hidden flex items-center justify-center">
+           <ChartNoAxesGantt onClick={toogleLogic} className="text-white w-[30px] h-[30px]"></ChartNoAxesGantt>
         </div>
+
+        </div>
+
+
+        
+
         </>
     )
 }
